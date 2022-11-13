07845ed024b060d60223f9861f5a5af8410cabef
Steven <steven.metry@icloud.com>
2022-11-14 10:28:22 +1300

    fixed spelling mistakes

--- components.js
+++ components.js
@@ -1,3 +1,8 @@
 function goToDiffrentPage(name) {
   window.open(`${name}.html`, "_self");
 }
+const sendQueryBtn = document.getElementById("sendQueryBtn");
+const contactForm = document.getElementById("contactForm")
+function sendQuery() {
+
+}
