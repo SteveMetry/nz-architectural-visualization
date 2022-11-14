bfe8955afee64a0e012dd71cb63966087470db71
Irsan Arisandy <irsanarisandy@hotmail.com>
2022-11-14 15:39:31 +1300

    Add contact query function and minor cleanups

--- components.js
+++ components.js
@@ -1,8 +1,15 @@
-function goToDiffrentPage(name) {
+function goToDifferentPage(name) {
   window.open(`${name}.html`, "_self");
 }
-const sendQueryBtn = document.getElementById("sendQueryBtn");
-const contactForm = document.getElementById("contactForm")
-function sendQuery() {
 
+function submitQuery(event) {
+  event.preventDefault();
+  const contactFormElements = document.getElementById("contactForm").elements;
+  const name = encodeURIComponent(contactFormElements['name'].value.trim());
+  const subject = encodeURIComponent(contactFormElements['subject'].value.trim() || `Query from ${name}`);
+  let body = contactFormElements['body'].value.trim();
+  if (name != "" && body != "") {
+    body = encodeURIComponent(`Enquiry: ${body}\n\nFrom: ${name}`);
+    window.location.href = `mailto:hello@nzarchviz.co.nz?subject=${subject}&body=${body}`;
+  }
 }
